b61baac6195c9f96874ec50fd7663c401da87763
Oliver Praesto <oliver.praesto@gmail.com>
2017-12-05 21:15:10 +0100

    - fixes first solution

--- day02/script.js
+++ day02/script.js
@@ -1,10 +1,38 @@
 var fs = require('fs');
 var inputFile = './input.txt';
-var input = fs.readFileSync(inputFile, 'utf8');
+var advInput = fs.readFileSync(inputFile, 'utf8');
 
 function checksum1(input) {
     var rows = input.split('\n');
-    console.log(rows);
+    var highest = 0;
+    var lowest = 9;
+    var sum = 0;
+
+    for(var r = 0; r < rows.length - 1; r++) {
+        var row = rows[r].split(/\s+/g);
+        console.log(row);
+
+        for (var c = 0; c < row.length; c++) {
+            var cell = row[c].split('');
+
+            for (var n = 0; n < cell.length; n++) {
+                var num = +cell[n];
+                var lastIndex = (cell.length - 1) === n;
+
+                highest = (num > highest) ? num : highest;
+                lowest = (num < lowest) ? num : lowest;
+
+                if (lastIndex) {
+                    sum += (highest - lowest);
+                    console.log(cell.join(''), highest + ' - ' + lowest + ' = ', (highest - lowest));
+                    highest = 0;
+                    lowest = 9;
+                }
+            }
+        }
+    }
+
+    console.log(sum);
 }
 
-checksum1(input);
+checksum1(advInput);
